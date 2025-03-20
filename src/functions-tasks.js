9481d635fe27fd148ba4bff97a0943bb3a22d15c
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-03-20 19:11:38 +0700

    feat: add part of solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -114,8 +114,9 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const result = func();
+  return () => result;
 }
 
 /**
@@ -133,8 +134,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let result;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        result = error;
+      }
+    }
+    throw result;
+  };
 }
 
 /**
